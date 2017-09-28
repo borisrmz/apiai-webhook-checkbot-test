f7114fbebfbde8bf7f9a02eec6778e39a7b29cdf
boris.jramirez <boris.aquino@gmail.com>
2017-09-27 21:09:31 -0600

    inicial

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php 
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+	switch ($text) {
+		case 'hoteles':
+			$speech = "Aqui va un listado de hoteles";
+			break;
+
+		case 'lugares':
+			$speech = "Aqui va un listado de lugares";
+			break;
+
+		case 'ayuda':
+			$speech = "Aqui la ayuda";
+			break;
+		
+		default:
+			$speech = "Perdón, no entendi esto. Pregunta sobre algo más";
+			break;
+	}
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+
+?>
